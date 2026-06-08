c50b2770688cb539c7c9ef143828877fe29ea305
Gabryelle Duarte <gabryelleddev@gmail.com>
2026-06-08 15:28:57 -0300

    Update SensoriamentoImpl.java

--- cliente/src/br/edu/ifba/satelites/clientes/impl/SensoriamentoImpl.java
+++ cliente/src/br/edu/ifba/satelites/clientes/impl/SensoriamentoImpl.java
@@ -7,8 +7,8 @@ import java.util.Random;
 import br.edu.ifba.satelites.clientes.sensoriamento.Sensoriamento;
 
 public class SensoriamentoImpl implements Sensoriamento<Leitura> {
-    private static final int TEMPERATURA_BRILHO_NORMAL = 60; // Temperatura ambiente típica das flores, em graus Celsius
-    private static final int OSCILACAO_MAXIMA_TEMPERATURA = 10;
+    private static final int TEMPERATURA_BRILHO_NORMAL = 47; // Temperatura ambiente típica das flores, em graus Celsius
+    private static final int ALTA_TEMPERATURA = 50;
     
     // Complexidade Geral: O(N)
     @Override
@@ -19,7 +19,7 @@ public class SensoriamentoImpl implements Sensoriamento<Leitura> {
         // O(N)
         for (int i = 0; i < totalLeituras; i++) {
             // O(1)
-            int oscilacao = TEMPERATURA_BRILHO_NORMAL * randomizador.nextInt(OSCILACAO_MAXIMA_TEMPERATURA)/100;
+            int oscilacao = TEMPERATURA_BRILHO_NORMAL * randomizador.nextInt(ALTA_TEMPERATURA)/100;
             int temperatura_de_brilho = randomizador.nextBoolean()? TEMPERATURA_BRILHO_NORMAL + oscilacao: TEMPERATURA_BRILHO_NORMAL - oscilacao;
             // O(1)
             boolean deteccao_fumaca = randomizador.nextBoolean();            
